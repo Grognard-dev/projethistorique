79997e81f24192cc201d181d6a4744aa68a02fde
Grognard-dev <lefevre.gregoire19@gmail.com>
2020-03-17 16:06:38 +0100

    travail d'aujourd'hui

--- index.php
+++ index.php
@@ -5,23 +5,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-
+    
     <!-- Font Google -->
     <link href="https://fonts.googleapis.com/css?family=Lato|Roboto|Spartan&display=swap" rel="stylesheet">
-
+    
     <!-- Own CSS -->
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/color.css">
-
-
+    
+    
 </head>
 
 <body>
-<<<<<<< HEAD
-    <h1>Grégoire</h1>
-=======
-oui kadder guillaume
->>>>>>> 73bc3e52343e7c66e3ce26d5bd04d397642c525f
+
+
+    
 </body>
 
 </html>
