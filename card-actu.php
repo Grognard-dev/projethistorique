a0d664b6803c055bff0a5023ba394cc118b2f1af
Grognard-dev <lefevre.gregoire19@gmail.com>
2020-03-18 16:51:36 +0100

    carrousel

---
+++ card-actu.php
@@ -0,0 +1,89 @@
+<html>
+  <head>
+  <title>My Now Amazing Webpage</title>
+  <script src="js/carrousel.js" async></script>
+  <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+<div class="container">
+
+    <div id="carousel1">
+
+      <div class="item">
+        <div class="item__image">
+          <img src="img/1.jpg" alt="">
+        </div>
+        <div class="item__body">
+          <div class="item__title">
+            Mon titre 1
+          </div>
+          <div class="item__description">
+            Ici une petite description pour tester le carousel
+          </div>
+        </div>
+      </div>
+
+      <div class="item">
+        <div class="item__image">
+          <img src="img/2.jpg" alt="">
+        </div>
+        <div class="item__body">
+          <div class="item__title">
+            Mon titre 2
+          </div>
+          <div class="item__description">
+            Ici une petite description pour tester le carousel
+          </div>
+        </div>
+      </div>
+
+      <div class="item">
+        <div class="item__image">
+          <img src="img/3.jpg" alt="">
+        </div>
+        <div class="item__body">
+          <div class="item__title">
+            Mon titre 3
+          </div>
+          <div class="item__description">
+            Ici une petite description pour tester le carousel
+          </div>
+        </div>
+      </div>
+
+      <div class="item">
+        <div class="item__image">
+          <img src="img/4.jpg" alt="">
+        </div>
+        <div class="item__body">
+          <div class="item__title">
+            Mon titre 4
+          </div>
+          <div class="item__description">
+            Ici une petite description pour tester le carousel
+          </div>
+        </div>
+      </div>
+
+      <div class="item">
+        <div class="item__body">
+          <div class="item__title">
+            Mon titre 5
+          </div>
+          <div class="item__description">
+            Ici une petite description pour tester le carousel
+          </div>
+        </div>
+      </div>
+
+    </div>
+    
+  <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+  <script type="text/javascript" src="js/slick.min.js"></script>
+  <script type="text/javascript" src="js/carrousel.js"></script>
+
+ 
+
+  </body>
+</html>
