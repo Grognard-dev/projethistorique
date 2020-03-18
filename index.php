47afbf3ba0edf3ec1470decf76e9da32d53f373b
guillaumepoucet <poucet@simplon-charleville.fr>
2020-03-18 16:51:12 +0100

    18/03/20

--- index.php
+++ index.php
@@ -7,25 +7,37 @@
     <title>Document</title>
 
     <!-- Font Google -->
-    <link href="https://fonts.googleapis.com/css?family=Lato|Roboto|Spartan&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lora|Lato|Roboto|Spartan&display=swap" rel="stylesheet">
 
     <!-- Own CSS -->
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/color.css">
 
-
 </head>
 
 <body>
     
-    <?php include ('php/nav.php'); ?>
-    <?php include ('php/header.php'); ?>
+    <!-- <?php include ('php/nav.php'); ?> -->
+    <!-- <?php include ('php/header.php'); ?> -->
+
+    <nav class="start">
+        <ul>
+            <a href="#">Accueil</a>
+            <a href="#">Mode d'emploi</a>
+            <a href="#">Application</a>
+            <a href="#">Contact</a>
+        </ul>
+    </nav>
+
+    <div style="height: 1000px; width: 100%; background-color: blue"></div>
 
 
+    <!-- <?php include ('php/footer.php'); ?> -->
 
-
-    <?php include ('php/footer.php'); ?>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="js/nav-js.js"></script>
 
 </body>
 
