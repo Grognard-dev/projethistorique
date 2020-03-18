c0fcc1c488ebf74fa0a59b9ca80ffb81010940a7
christophesd <christophe.sd@gmail.com>
2020-03-18 17:26:27 +0100

    home mobile

--- index.php
+++ index.php
@@ -7,12 +7,12 @@
     <title>Document</title>
 
     <!-- Font Google -->
-    <link href="https://fonts.googleapis.com/css?family=Lato|Roboto|Spartan&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700|Lora:400,700|Roboto:300,400,500,700|Spartan:300,400,500,700&display=swap" rel="stylesheet">
 
     <!-- Own CSS -->
     <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/color.css">
+    <link rel="stylesheet" href="css/style.css">
 
 
 </head>
@@ -22,8 +22,66 @@
     <?php include ('php/nav.php'); ?>
     <?php include ('php/header.php'); ?>
 
+    <main>
+        <section class="bloc">
+
+            <div class="img">
+                <img src="ressources/img/arcade.png" alt="">
+            </div>
+
+            <div class="text-light bignb">01</div>
+
+            <h2 class="titlebg">Contexte</h2>
+
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit a quaerat maxime odit quia aut vitae, fugit tempore ipsa, quod reprehenderit dolore! Sit vitae quam, rerum explicabo vel delectus assumenda!</p>
 
 
+        </section>
+        <section class="bloc bg-light">
+
+            <div class="img">
+                <img src="ressources/img/archives08.jpeg" alt="">
+            </div>
+
+            <div class="text-white bignb">02</div>
+
+            <h2 class="titlelw">Archives Départementales des Ardennes</h2>
+
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit a quaerat maxime odit quia aut vitae, fugit tempore ipsa, quod reprehenderit dolore! Sit vitae quam, rerum explicabo vel delectus assumenda! Suscipit a quaerat maxime odit quia aut vitae, fugit tempore ipsa, quod reprehenderit dolore! Sit vitae quam, rerum explicabo vel delectus assumenda!</p>
+
+        </section>
+        <section class="bloc">
+
+            <div class="img">
+                <img src="ressources/img/gonzague.png" alt="">
+            </div>
+
+            <div class="text-gold bignb">03</div>
+
+            <h2 class="titlelg">Le travail du CNRS</h2>
+
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit a quaerat maxime odit quia aut vitae, fugit tempore ipsa, quod reprehenderit dolore! Sit vitae quam, rerum explicabo vel delectus assumenda! Suscipit a quaerat maxime odit quia aut vitae, fugit tempore ipsa, quod reprehenderit dolore! Sit vitae quam, rerum explicabo vel delectus assumenda!</p>
+
+
+        </section>
+        <section class="bloc bg-gold">
+
+
+            <div class="text-white bignb">04</div>
+
+            <h2 class="titlebw">Application</h2>
+
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit a quaerat maxime odit quia aut vitae, fugit tempore ipsa, quod reprehenderit dolore! Sit vitae quam, rerum explicabo vel delectus assumenda!</p>
+
+
+            <div class="btm text-black">
+                <a class="bg-white" href="">Mode d'emploi</a>
+                <a class="bg-white" href="">C'est parti</a>
+            </div>
+
+
+        </section>
+    </main>
 
     <?php include ('php/footer.php'); ?>
 
