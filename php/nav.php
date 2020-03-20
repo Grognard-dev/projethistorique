8ebc383c9bdb6cae01c778708c8d50a685528fd7
guillaumepoucet <poucet@simplon-charleville.fr>
2020-03-20 16:35:07 +0100

    nav + header ok

--- php/nav.php
+++ php/nav.php
@@ -1,8 +1,8 @@
 <nav class="topnav start" id="myTopnav">
-    <a href="#" class="active">Accueil</a>
-    <a href="#">Mode d'emploi</a>
-    <a href="#">Application</a>
-    <a href="#">Contact</a>
+    <a href="../index.php" class="underline active">Accueil</a>
+    <a href="php/manual.php" class="underline">Mode d'emploi</a>
+    <a href="php/application.php" class="underline">Application</a>
+    <a href="#" class="underline">Contact</a>
     <a href="javascript:void(0);" class="icon" onclick="myFunction()">
         <i class="fa fa-bars"></i>
     </a>
