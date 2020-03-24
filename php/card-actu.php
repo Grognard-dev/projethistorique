3be1808058229178f17ac215c2eaf28a732c6bb2
christophesd <christophe.sd@gmail.com>
2020-03-24 22:12:26 +0100

    final V1 22:12

---
+++ php/card-actu.php
@@ -0,0 +1,145 @@
+<section id="actu" class="bloc bg-light">
+    <h2 class="titlebw">Actualité</h2>
+    <div>
+        <div id="carousel1">
+            <div class="item">
+                <div class="item__image" style="background:url(ressources/img/photo1.jpg); "></div>
+                <div class="item__body">
+                    <div class="item__title">
+                        Mon titre 1
+                    </div>
+                    <div class="item__description">
+                        Ici une petite description pour tester le carousel
+                    </div>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="item__image" style="background:url(ressources/img/photo2.jpg); "></div>
+                <div class="item__body">
+                    <div class="item__title">
+                        Mon titre 2
+                    </div>
+                    <div class="item__description">
+                        Ici une petite description pour tester le carousel
+                    </div>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="item__image" style="background:url(ressources/img/photo3.jpg);"></div>
+                <div class="item__body">
+                    <div class="item__title">
+                        Mon titre 3
+                    </div>
+                    <div class="item__description">
+                        Ici une petite description pour tester le carousel
+                    </div>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="item__image" style="background:url(ressources/img/photo4.png); "></div>
+                <div class="item__body">
+                    <div class="item__title">
+                        Mon titre 4
+                    </div>
+                    <div class="item__description">
+                        Ici une petite description pour tester le carousel
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="item__image" style="background:url(ressources/img/photo1.jpg); "></div>
+                <div class="item__body">
+                    <div class="item__title">
+                        Mon titre 1
+                    </div>
+                    <div class="item__description">
+                        Ici une petite description pour tester le carousel
+                    </div>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="item__image" style="background:url(ressources/img/photo2.jpg); "></div>
+                <div class="item__body">
+                    <div class="item__title">
+                        Mon titre 2
+                    </div>
+                    <div class="item__description">
+                        Ici une petite description pour tester le carousel
+                    </div>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="item__image" style="background:url(ressources/img/photo3.jpg); "></div>
+                <div class="item__body">
+                    <div class="item__title">
+                        Mon titre 3
+                    </div>
+                    <div class="item__description">
+                        Ici une petite description pour tester le carousel
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="item__image" style="background:url(ressources/img/photo3.jpg); "></div>
+                <div class="item__body">
+                    <div class="item__title">
+                        Mon titre 3
+                    </div>
+                    <div class="item__description">
+                        Ici une petite description pour tester le carousel
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="item__image" style="background:url(ressources/img/photo3.jpg); "></div>
+                <div class="item__body">
+                    <div class="item__title">
+                        Mon titre 3
+                    </div>
+                    <div class="item__description">
+                        Ici une petite description pour tester le carousel
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="item__image" style="background:url(ressources/img/photo3.jpg); "></div>
+                <div class="item__body">
+                    <div class="item__title">
+                        Mon titre 3
+                    </div>
+                    <div class="item__description">
+                        Ici une petite description pour tester le carousel
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="item__image" style="background:url(ressources/img/photo3.jpg); "></div>
+                <div class="item__body">
+                    <div class="item__title">
+                        Mon titre 3
+                    </div>
+                    <div class="item__description">
+                        Ici une petite description pour tester le carousel
+                    </div>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="item__image" style="background:url(ressources/img/photo4.png); "></div>
+                <div class="item__body">
+                    <div class="item__title">
+                        Mon titre 4
+                    </div>
+                    <div class="item__description">
+                        Ici une petite description pour tester le carousel
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ php/footer.php
@@ -0,0 +1,15 @@
+<footer>
+    <div class="footer1">
+        <div>
+            <div><img src="ressources/img/cnil.svg"></div>
+            <div><img class="smallimg" src="ressources/img/cnrs.png"></div>
+        </div>
+        <div>
+            <div><img src="ressources/img/grandest.png"></div>
+            <div><img src="ressources/img/ardennesmetropole.png"></div>
+        </div>
+    </div>
+
+
+    <div class="legal"><a href="index.php">Mentions légales</a></div>
+</footer>
