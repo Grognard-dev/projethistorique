488a1f4d2e87a8def94b0ee69b7d9d49939f0c0c
christophesd <christophe.sd@gmail.com>
2020-05-13 10:58:48 +0200

    ffggt

--- php/header.php
+++
@@ -1,6 +0,0 @@
-<header class="text-white">
-    <h1>Charleville-Mézières</h1>
-    <hr class="bg-light">
-    <p>Plongez-vous dans plus de 200 ans d'histoire et découvrez la vie des habitants de la cité
-        carolomacérienne entre 1700 et 1900</p>
-</header>
